e4ffbee7d20e77f2a600a758989974f50e64a3f3
Rodrigo Pichilingue <rv7071597@gmail.com>
2025-10-25 21:04:01 -0500

    css y js

---
+++ dist/Js/scripts.js
@@ -0,0 +1,26 @@
+function mostrarSeccion(id) 
+{
+      const secciones = document.querySelectorAll('.seccion');
+      secciones.forEach(sec => sec.classList.remove('visible'));
+      document.getElementById(id).classList.add('visible');
+} 
+ 
+/* ---------- CARRUSEL AUTOMÁTICO ---------- */
+
+let slideIndex = 0;
+
+function mostrarSlideAutomatico() {
+  const slides = document.querySelectorAll('.carousel-item');
+
+  slides.forEach(slide => slide.classList.remove('visible'));
+
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+
+  slides[slideIndex - 1].classList.add('visible');
+}
+
+/* Tiempo: cada 4 segundos (puedes cambiarlo) */
+setInterval(mostrarSlideAutomatico, 4000);
+
+    
